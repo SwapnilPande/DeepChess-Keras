c978b73d2f3bdaff5db87d708cb1d1aa9da661a5
Swapnil Pande <swapnil.pande98@gmail.com>
2019-04-10 12:14:52 -0500

    Added Keras sequence generator to trainfile

--- trainDeepChess.py
+++ trainDeepChess.py
@@ -1,6 +1,7 @@
-from keras.layers import Dense, Input
-from keras.models import Model
-from keras import backend as K
+# from keras.layers import Dense, Input, Concatenate
+# from keras.models import Model, load_model
+# from keras.utils import Sequence
+# from keras import backend as K
 
 import numpy as np
 
@@ -12,45 +13,105 @@ deepchessLayers = [400, 200, 100, 2]
 # Path to Pos2Vec Model
 pos2VecPath = "./data/Pos2Vec.h5"
 
-numWhites = 1000000
-numBlacks = 1000000
 
-# Retrieve input data from files
-print("Loading data from files:")
-print("\t" + whiteBoardsFile)
-print("\t" + blackBoardsFile)
+class DeepchessDataGenerator:
+    def __init__(self, batchSize):
+        self.batchSize = batchSize
 
-# Load black and white boards from file
-whites = np.load(whiteBoardsFile)
-blacks = np.load(blackBoardsFile)
+        self.whiteBoardsFile = "./data/whites.npy"
+        self.blackBoardsFile = "./data/black.npy"
 
-print("Successfully loaded data")
-print()
+        # Size of dataset
+        self.numWhites = 1000000
+        self.numBlacks = 1000000
+
+        # Retrieve input data from files
+        print("DATA GENERATOR: Loading data from files:")
+        print("\t" + self.whiteBoardsFile)
+        print("\t" + self.blackBoardsFile)
+
+        # Load black and white boards from file
+        self.whites = np.zeros((1000, 773))#np.load(whiteBoardsFile)
+        self.blacks = np.ones((1000, 773))#np.load(blackBoardsFile)
+
+        # Randomize order of each dataset
+        np.random.shuffle(self.whites)
+        np.random.shuffle(self.blacks)
+
+        print("DATA GENERATOR: Successfully loaded data")
+        print()
+
+    def __len__(self):
+        return int(np.ceil(self.numWhites / float(self.batchSize)))
+
+    def __getitem__(self, index):
+        startIndex = index*self.batchSize
+        print(startIndex)
+
+        try: #Full size batch
+            whiteBatch = self.whites[startIndex : startIndex + self.batchSize]
+            blackBatch = self.blacks[startIndex : startIndex + self.batchSize]
+        except IndexError: #Retrieve small batch at the end of the array
+            whiteBatch = self.whites[startIndex:]
+            blackBatch = self.blacks[startIndex:]
+
+        # Generate labels
+        whiteLabels = np.ones((self.batchSize,))
+        blackLabels = np.zeros((self.batchSize,))
+
+        #Create arrays for batchs
+        x = np.stack([whiteBatch, blackBatch], axis = 1)
+        labels = np.stack([whiteLabels, blackLabels], axis = 1)
+
+        # Randomly switch white and black board
+        # Randomly generate array of 1 and 0 of length batchSize
+        # Switch each index that contains 1
+        swapIndices = np.random.randint(2, size = self.batchSize)
+        x[swapIndices == 1] = np.flip(x[swapIndices == 1], axis = 1)
+        labels[swapIndices == 1] = np.flip(labels[swapIndices == 1], axis = 1)
+
+        return x, labels
+
+    # Shuffle the order of the white and blacks
+    def on_epoch_end(self):
+        print("DATA GENERATOR: Shuffled white and black boards")
+        np.shuffle(self.whites)
+        np.shuffle(self.blacks)
+
+
 
 # List to store weights and biases from autoencoder
 weights = []
 
+print("------------ Initialize Data Generator ------------")
+trainGenerator = DeepchessDataGenerator
 
 print("------------ Beginning training ------------")
+# Load pos2Vec model
+pos2Vec = load_model(pos2VecPath)
+
 # Inputs to Pos2Vec networks
 inputLeft = Input(shape=(773,))
 inputRight = Input(shape=(773,))
-pos2VecLeft =
 
-ae1 = Dense(autoencoderLayers[1], activation='relu',trainable=False)(inputLayer)
-ae2 = Dense(autoencoderLayers[2], activation='relu',trainable=False)(ae1)
-ae3 = Dense(autoencoderLayers[3], activation='relu',trainable=False)(ae2)
-outputLayer = Dense(autoencoderLayers[4], activation='relu',trainable=False)(ae3)
-ae = Model(inputLayer, outputLayer)
+# Outputs from siamese pos2Vec networks
+pos2VecLeftOut = pos2Vec(inputLeft)
+pos2VecRightOut = pos2Vec(inputRight)
+pos2VecOut = Concatenate()([pos2VecLeftOut, pos2VecRightOut])
 
-# Iterate over autoencoder model - skip over input layer
-for layer, weight in zip(ae.layers[1:], weights):
-    # Set weights for each layer
-    layer.set_weights(weight)
+# Build deepchess network
+deepchess1 = Dense(deepchessLayers[0], activation='relu')(pos2VecOut)
+deepchess2 = Dense(deepchessLayers[1], activation='relu')(deepchess1)
+deepchess3 = Dense(deepchessLayers[2], activation='relu')(deepchess2)
+deepchessOut = Dense(deepchessLayers[3], activation='softmax')(deepchess3)
 
-# Save final model
-savePath = os.path.join(weightsPath, "AutoencoderWeights.h5")
-ae.save(savePath)
+# Compile model
+deepchess = Model([inputLeft, inputRight], deepchessOut)
+deepchess.compile(optimizer = 'adam', loss='categorical_crossentropy', metrics = ['acc'])
+
+deepchess.fit_generator(trainGenerator, epochs = 50)
+
+deepchess.save("./data/DeepchessModel.h5")
 
 
 
